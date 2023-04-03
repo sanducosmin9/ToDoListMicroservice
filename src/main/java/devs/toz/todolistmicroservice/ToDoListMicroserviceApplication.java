9bbb6d5e32b8c800d204dc53996d652cb040c79d
sanducosmin9 <56782613+sanducosmin9@users.noreply.github.com>
2023-04-03 15:44:31 +0300

    create project

---
+++ src/main/java/devs/toz/todolistmicroservice/ToDoListMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package devs.toz.todolistmicroservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToDoListMicroserviceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ToDoListMicroserviceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/devs/toz/todolistmicroservice/ToDoListMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package devs.toz.todolistmicroservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToDoListMicroserviceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
