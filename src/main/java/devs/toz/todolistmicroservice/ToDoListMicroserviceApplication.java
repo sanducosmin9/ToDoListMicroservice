eeb3168889e212cbf9ee73ed57f1da7e055af0a6
Robert-Vieriu <robert-gabriel.vieriu21@student.ub.ro>
2023-04-04 15:54:47 +0300

    Create a basic working endpoint

--- src/main/java/devs/toz/todolistmicroservice/ToDoListMicroserviceApplication.java
+++ src/main/java/devs/toz/todolistmicroservice/ToDoListMicroserviceApplication.java
@@ -2,12 +2,20 @@ package devs.toz.todolistmicroservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class ToDoListMicroserviceApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ToDoListMicroserviceApplication.class, args);
     }
 
+    @GetMapping("/")
+    public String working()
+    {
+        return "I am working";
+    }
 }
